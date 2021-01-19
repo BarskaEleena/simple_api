25b13cef64c16c244cb943a4ea6cdf78cc77d3ca
Helen <71237788+HelenBarska@users.noreply.github.com>
2021-01-19 15:46:09 +0200

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -3,9 +3,16 @@ from django.http import HttpResponse
 
 from .models import Greeting
 
+import random
+from django.http import JsonResponse
+
 # Create your views here.
 def index(request):
-    return HttpResponse('Chatbot answer.')
+    rand = ['a', 'b', 'c', 'd', 'e']
+    #random.choice(foo))
+    rand = random.choice(rand))
+    data = [{"answer": rand}]
+    return JsonResponse(data, safe=False)
     #return render(request, "index.html")
 
 
